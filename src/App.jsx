11020dd2eb285b8fc42b2391dfe2001939584a87
hashimon000 <hashimon.hashimon@icloud.com>
2023-05-12 09:55:28 +0900

    期限機能の追加機能typeの属性によって付与した

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import React, { useState } from "react";
 export const App=()=> {
   const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState("");
- 
+  const [dueDateInput, setDueDateInput] = useState("");
   const handleAddTodo = () => {
     if (todoInput !== "") {
       setTodos([...todos, { task: todoInput,  }]);
       setTodoInput("");
+      setDueDateInput("");
       
     }
   };
@@ -22,7 +23,13 @@ export const App=()=> {
         type="text"
         placeholder="タスクを入力してください"
         value={todoInput}
-        onChange={(e) => setTodoInput(e.target.value)}
+        onChange={(a) => setTodoInput(a.target.value)}
+      />
+      <input
+        type="date"
+        placeholder="期限を入力してください"
+        value={dueDateInput}
+        onChange={(a) => setDueDateInput(a.target.value)}
       />
        <button onClick={handleAddTodo}>追加</button>
       
